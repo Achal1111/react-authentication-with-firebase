c685d8dd7ee0dbf2fdd787e71a77f103d1c0e81f
Achal1111 <achal.mehta@dripcapital.com>
2023-04-17 12:43:59 +0530

    Added context

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
+import { Container } from "react-bootstrap";
 import Signup from "./Signup";
+import { AuthProvider } from "../contexts/AuthContext";
 
 function App() {
   return (
-    <div className="App">
-      <Signup />
+    <AuthProvider>
+      <div className="App">
+      <Container className="d-flex align-items-center justify-content-center" style={{minHeight:'100vh'}}>
+        <div className="w-100" style={{maxWidth:'400px'}}>
+        <Signup />
+        </div>
+      </Container>
+     
     </div>
+    </AuthProvider>
+    
   );
 }
 
