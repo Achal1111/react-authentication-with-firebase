1ff4a1e44b86af4d62f946683d7a76508ea20417
Achal1111 <achal.mehta@dripcapital.com>
2023-04-17 13:00:06 +0530

    Error Handling

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,21 +1,34 @@
-import React, { useRef } from "react";
-import { Form, Button, Card } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
   const { signup } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
-    signup(emailRef.current.value, passwordRef.current.value);
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match ");
+    }
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("Failed to create an account");
+    }
+    setLoading(false);
   }
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign up</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
@@ -39,7 +52,7 @@ export default function Signup() {
                 required
               ></Form.Control>
             </Form.Group>
-            <Button className="w-100 mt-4" type="submit">
+            <Button disabled={loading} className="w-100 mt-4" type="submit">
               Sign Up
             </Button>
           </Form>
